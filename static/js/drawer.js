b614609d140f3358e08e933b9fcada5f151116f0
Adam Joyce <adamjoyce@outlook.com>
2018-04-02 13:40:26 +0100

    Project panes toggling correctly + page scroll

--- static/js/drawer.js
+++ static/js/drawer.js
@@ -30,15 +30,40 @@ var Drawer = {
     // Project panes.
     var toggleElements = document.getElementsByClassName('project');
     if (toggleElements) {
+      // Grab all potential project drawers.
+      var drawerElements = document.querySelectorAll('[id*="project-pane-"]');
       for (var i = 0; i < toggleElements.length; ++i) {
-        var drawerElement = document.getElementById(
-          toggleElements[i].dataset.drawer
-        );
-        toggleElements[i].addEventListener('click', function() {
-          drawerElement.classList.toggle('hidden');
-          console.log("CLICKED + " + drawerElement.id);
+        toggleElements[i].addEventListener('click', function(e) {
+          var targetDrawer = document.getElementById(
+            e.target.parentNode.dataset.drawer
+          );
+          if (targetDrawer) {
+            // Closes any currently opened drawers that are not the target.
+            for (var i = 0; i < drawerElements.length; ++i) {
+              if (drawerElements[i] !== targetDrawer &&
+                  !drawerElements[i].classList.contains('hidden')) {
+                    drawerElements[i].classList.toggle('hidden');
+                  }
+            }
+
+            // Toggle the target open if closed.
+            if (targetDrawer.classList.contains('hidden')) {
+              targetDrawer.classList.toggle('hidden');
+            }
+            Drawer.smoothScrollToTop();
+          }
         });
       }
+    }
+  },
+
+  smoothScrollToTop: function() {
+    var currentScroll = document.documentElement.scrollTop ||
+                        document.body.scrollTop;
+    if (currentScroll > 0) {
+      // Smoothly animate a scroll to the top.
+      window.requestAnimationFrame(Drawer.smoothScrollToTop);
+      window.scrollTo(0, currentScroll - (currentScroll / 5));
     }
   }
 };
