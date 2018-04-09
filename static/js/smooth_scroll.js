3b1869c9166bbc91c3cb7571beeeb3432b065f17
Adam Joyce <adamjoyce@outlook.com>
2018-04-09 12:06:46 +0100

    Scrollbar disabled during smooth scroll to top when switching drawers

--- static/js/smooth_scroll.js
+++ static/js/smooth_scroll.js
@@ -1,11 +1,22 @@
 var SmoothScroll = {
+  maxWidthSet: false,
   scrollToTop: function() {
+    if (!SmoothScroll.maxWidthSet) {
+      document.body.style.maxWidth = document.body.clientWidth + 'px';
+      document.body.style.overflowY = 'hidden';
+      SmoothScroll.maxWidthSet = true;
+    }
     var currentScroll = document.documentElement.scrollTop ||
                         document.body.scrollTop;
     if (currentScroll > 0) {
       // Smoothly animate a scroll to the top.
       window.requestAnimationFrame(SmoothScroll.scrollToTop);
       window.scrollTo(0, currentScroll - (currentScroll / 5));
+    }
+    else {
+      document.body.style.maxWidth = 'none';
+      document.body.style.overflowY = 'visible';
+      SmoothScroll.maxWidthSet = false;
     }
   }
 }
