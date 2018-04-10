e62b235a61df1425eb3e0a88b0438109219ca636
Adam Joyce <adamjoyce@outlook.com>
2018-04-10 11:51:53 +0100

    CV now grabbed via MEDIA_ROOT

--- adam_joyce_games_portfolio/views.py
+++ adam_joyce_games_portfolio/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 def about(request):
     return render(request, 'about.html')
 
 def cv(request):
-    with open('media/pdf/adam_joyce_cv_v5.pdf', 'rb') as pdf:
+    with open(settings.MEDIA_ROOT + '/pdf/adam_joyce_cv_v5.pdf', 'rb') as pdf:
         response = HttpResponse(pdf.read(), content_type='application/pdf')
         response['Content-Disposition'] = ('inline;'
                                            'filename=adam_joyce_cv_v5.pdf')
